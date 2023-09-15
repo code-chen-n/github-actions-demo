6419a1a5622efd5005413c02e4a3180ee113a231
ChenJinTong <2050407860@qq.com>
2023-09-16 04:12:55 +0800

    update 9.16:v1

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+  <div class="header">Welcome to my test html!</div>
+
+  <h1>github-actions  测试</h1>
+
+  <h2>第一次使用github-actions</h2>
+
+
+</template>
+
+<style scoped>
+
+.header{
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 8vh;
+  width: 100vw;
+  background-color: aquamarine;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  color: blue;
+}
+
+</style>
